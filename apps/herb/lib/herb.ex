e1b32ad7b865de69da220b432ec3a12848297b1d
kc2853 <kc2853@columbia.edu>
2021-03-21 20:01:32 +0900

    DKG helper functions

--- apps/herb/lib/herb.ex
+++ apps/herb/lib/herb.ex
@@ -43,4 +43,118 @@ defmodule Herb do
     # Byzantine nodes (for demonstration purposes)
     byzantine: nil
   )
+
+  def new_configuration(
+        t,
+        n,
+        g,
+        p,
+        view,
+        round_max,
+        last_output
+      ) do
+    q = trunc((p - 1) / 2)
+    view_id = Enum.with_index(view, 1) |> Map.new()
+    view_subshare = Enum.map(view, fn a -> {a, nil} end) |> Map.new()
+    view_subsign = 1..round_max |> Enum.map(fn n ->
+                                     {n, Enum.map(view, fn a -> {a, nil} end) |> Map.new()}
+                                   end)
+                                |> Map.new()
+    %Herb{
+      t: t,
+      n: n,
+      g: g,
+      p: p,
+      q: q,
+      view: view,
+      view_id: view_id,
+      view_subshare: view_subshare,
+      view_subsign: view_subsign,
+      round_max: round_max,
+      round_current: 0,
+      last_output: last_output,
+      replier: false,
+      byzantine: false
+    }
+  end
+
+  # Generator (primitive root) of Z_q where q is a prime number equal to (p - 1) / 2
+  def get_generator(p) do
+    get_generator(p, 2)
+  end
+
+  # Algorithm 4.86 from http://cacr.uwaterloo.ca/hac/about/chap4.pdf
+  defp get_generator(p, x) do
+    cond do
+      # Find a generator of (Z_p)* first
+      # then square it to get a generator of Z_q
+      :maths.mod_exp(x, 2, p) != 1 && :maths.mod_exp(x, trunc((p - 1) / 2), p) != 1 ->
+        # IO.puts "Generator: #{inspect(:maths.mod_exp(x, 2, p))}"
+        :maths.mod_exp(x, 2, p)
+      true ->
+        get_generator(p, x + 1)
+    end
+  end
+
+  # We start n parallel instances of VSS (verifiable secret sharing)
+  def get_poly_then_send(state) do
+    # Generate t number of random coefficients in (Z_p)*
+    coeff = Enum.map(1..state.t, fn _ -> :rand.uniform(state.p - 1) end)
+    comm = get_comm(coeff, state.g, state.p)
+
+    # Correctly send corresponding subshares to each node
+    state.view
+    |> Enum.filter(fn pid -> pid != whoami() end)
+    |> Enum.map(fn pid ->
+         id = Map.get(state.view_id, pid)
+         subshare = get_subshare(coeff, id)
+         msg = {subshare, comm}
+         send(pid, msg)
+       end)
+
+    # Calculate my subshare and update state
+    id_me = Map.get(state.view_id, whoami())
+    subshare_me = get_subshare(coeff, id_me)
+    state = %{state | view_subshare: Map.put(state.view_subshare, whoami(), subshare_me)}
+    state
+  end
+
+  # We commit to coefficients by raising g (group generator) to the power of each coefficient
+  def get_comm(coeff, g, p) do
+    # :maths belongs to ndpar library
+    Enum.map(coeff, fn x -> :maths.mod_exp(g, x, p) end)
+  end
+
+  # Horner's method for polynomial evaluation (at id)
+  def get_subshare(coeff, id) do
+    Enum.reduce(Enum.reverse(coeff), 0, fn x, acc -> x + acc * id end)
+  end
+
+  # Verify a subshare as per VSS (verifiable secret sharing)
+  def verify_subshare(subshare, comm, g, p, id) do
+    lhs = :maths.mod_exp(g, subshare, p)
+    rhs = Enum.with_index(comm)
+    rhs = Enum.map(rhs, fn t -> :maths.mod_exp(elem(t, 0), :maths.pow(id, elem(t, 1)), p) end)
+    rhs = Enum.reduce(rhs, fn x, acc -> :maths.mod(x * acc, p) end)
+    lhs == rhs
+  end
+
+  # Above are utility functions before DKG
+  # Below is DKG
+
+  # Distributed key generation
+  def dkg(state) do
+    dkg(state, 0)
+  end
+
+  # Counter counts how many subshares one has received so far (need n)
+  # Note (QUAL assumption): In the literature, the usual assumption is that some nodes could be
+  # unresponsive/faulty/Byzantine in the DKG phase (pre-DRB phase), in which case
+  # nodes first need to agree on a group of qualified nodes (denoted by QUAL) during DKG.
+  # Here, we assume that all initialized nodes are honest and fully functional
+  # in the DKG phase (perhaps not in the DRB phase later though). In other words,
+  # all nodes are in QUAL, so the number of nodes in QUAL is n.
+  defp dkg(state, counter) do
+    nil
+  end
 end
